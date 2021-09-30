1b61362b3cfb0b03490f8b0ef1b1f5fa7ceb1cc1
Peter Jandovitz <plj1280@gmail.com>
2021-09-29 20:16:22 -0400

    added delete task from db

--- src/App.js
+++ src/App.js
@@ -3,62 +3,89 @@ import {useState, useEffect} from 'react'
 
 function App() {
 
+  const dbURL = "https://u34x82trhl.execute-api.us-east-2.amazonaws.com/Prod/tasks"
   const [tasks, setTasks] = useState([]);
   const [textInput, setTextInput] = useState("");
 
+  function strToNum(key,val) {
+    const num = Number(val)
+    return isNaN(num) ? val : num
+  }
+
   const onClickCreateTask = () => {
     var req = new XMLHttpRequest();
-    req.open("GET","https://u34x82trhl.execute-api.us-east-2.amazonaws.com/Prod/tasks");
+    req.open("POST",dbURL);
+    const newtask = {
+      taskName : textInput,
+      username : "plj1280",
+      targetTime : 0
+    }
     req.onload = function() {
-      var response = JSON.parse(this.responseText);
-      console.log(response);
-
+      var task = JSON.parse(this.responseText,strToNum)
       setTextInput("");
-      setTasks(
-      [...tasks, 
-      {
-      description : response[0].taskName.S,
-      progress : 0,
-      active : false,
-      startTime : 0,
-      savedProgress : 0,
-      onClickSetActive : ()=>{
-        setTasks(ptasks=>ptasks.map((task,i)=>((i===(tasks.length)) ? {
-          ...task, active:!(task.active),
-          startTime : !(task.active) ? (new Date().getTime()) : null,
-          savedProgress : task.active ? task.progress : task.savedProgress
-        } : {...task, active:false, savedProgress:task.progress}
-        )))
+      setTasks([...tasks,
+        {...task,
+          onClickDeleteTask : onClickDeleteTaskGen(task),
+          onClickSetActive : onClickSetActiveGen(task)
+        }
+      ])
+    }
+  req.send(JSON.stringify({method:"POST",item:newtask}));
+  }
 
+
+  function onClickDeleteTaskGen(task) {
+    return () => {
+      setTasks(ptasks=>ptasks.filter(ptask=>(ptask.creationTime!==task.creationTime)))
+      var delreq = new XMLHttpRequest()
+      delreq.open("POST",dbURL)
+      delreq.send(JSON.stringify({method : "DELETE", item : task}))
+    }
+  }
+
+  function onClickSetActiveGen(task) {
+    return () => {
+      setTasks(ptasks=>ptasks.map(ptask=>(ptask.creationTime===task.creationTime ? {
+        ...ptask,
+        active : !ptask.active,
+        startTime : ptask.active ? ptask.startTime : new Date().getTime(),
+        elapsedTime : ptask.active ? ptask.progress : ptask.elapsedTime
+      } : {
+        ...ptask, active : false, elapsedTime : ptask.active ? ptask.progress : ptask.elapsedTime
       }
-      }])}
-  req.send();
-  };
+      )))
+    }
+  }
 
-  useEffect(()=>setInterval(()=>setTasks(ptasks=>ptasks.map((task)=>(
-    task.active ? {...task, progress : task.savedProgress + ((new Date().getTime())-task.startTime)/100} : task
-  )
-    )), 100),[])
-
-  function getTasksFromDB() {
+  useEffect(function () {
     var req = new XMLHttpRequest();
-    req.open("GET","https://u34x82trhl.execute-api.us-east-2.amazonaws.com/Prod/tasks");
+    req.open("GET",dbURL);
     req.onload = function () {
-      setTasks(JSON.parse(this.responseText))
+      var dbTasks = JSON.parse(this.responseText,strToNum)
+      setTasks(dbTasks.map(function (item) {
+        return {
+          ...item,
+          onClickDeleteTask : onClickDeleteTaskGen(item),
+          onClickSetActive : onClickSetActiveGen(item)
+        }
+      }))
     }
     req.send()
-  }
-  useEffect(()=>{
-    getTasksFromDB()
-  },[])
+  }, [])
 
 
+
+  useEffect(()=>setInterval(()=>{
+    setTasks(ptasks=>ptasks.map((task)=>{
+    //task.active ? {...task, progress : task.savedProgress + ((new Date().getTime())-task.startTime)/100} : task
+      return task.active ? {...task, progress:task.elapsedTime+(new Date().getTime()-task.startTime)/100} : task
+    }))}, 100),[])
 
 
   return (
     <div className="App">
       Hello.
-      {tasks.map((task)=> <Task task={task}></Task>)}
+      {tasks.map((task)=> <Task task={task} key={task.creationTime}></Task>)}
           New task
           <input type="text" value={textInput} onChange={(event)=>setTextInput(event.target.value)}/>
       <button onClick={onClickCreateTask}>New task</button>
--- src/Task.js
+++ src/Task.js
@@ -7,11 +7,16 @@ function Task(props) {
         height : "2em",
         backgroundColor : props.task.active ? "#2277bb" : "#bb7722"
     }
+    const delButtonStyle = {
+        width : "5em",
+        height : "2em",
+    }
 
     return (
         <div>
             Task: {props.task.taskName}
             <button className='playButton' style={buttonStyle} onClick={props.task.onClickSetActive}>Start</button>
+            <button style={delButtonStyle} onClick={props.task.onClickDeleteTask}>Delete</button>
             <ProgressBar progress={props.task.progress}></ProgressBar>
         </div>
     );
